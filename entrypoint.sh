8baff76d7033ae2e62153ef91d253363db4b6a9f
Austin Dimmer <austin@effective-computing.com>
2020-10-13 12:09:40 +0200

    explicitly define cdk-assume-role-plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,8 +65,8 @@ function installPipRequirements(){
 }
 
 function runCdk(){
-	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\" --app \"${INPUT_CDK_STACK_SOURCE}\""
-	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" --app "${INPUT_CDK_STACK_SOURCE}" -v 2>&1)
+	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} --app \"${INPUT_CDK_STACK_SOURCE}\" -v"
+	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" --app \"${INPUT_CDK_STACK_SOURCE}\" --plugin cdk-assume-role-credential-plugin -v 2>&1)
 	exitCode=${?}
 	echo ::set-output name=status_code::${exitCode}
 	echo "${output}"
